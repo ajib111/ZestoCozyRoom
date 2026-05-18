f3c09828f80bbefc0c3c3f89c7255d4fbfea24cd
ajib111 <dahalajib99@gmail.com>
2026-05-18 23:49:01 +0545

    Added Mr.Ajib

---
+++ src/components/Ajib.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import atPhone from "../assets/AjibAtPhone.png";
+import waving from "../assets/AjibWaving.png";
+
+function Ajib() {
+  const [isWaving, setIsWaving] = useState(false);
+
+  const handleClick = () => {
+    setIsWaving(true);
+
+    setTimeout(() => {
+      setIsWaving(false);
+    }, 2000);
+  };
+
+  return (
+    <img
+      src={isWaving ? waving : atPhone}
+      alt="Ajib"
+      onClick={handleClick}
+style={{
+  position: "absolute",
+  bottom: "0%",
+  right: "-25%",
+  width: "320px",
+  zIndex: 10,
+  cursor: "pointer",
+  userSelect: "none",
+}}
+    />
+  );
+}
+
+export default Ajib;
--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -3,23 +3,30 @@ import { useState } from "react";
 import dayBg from "../assets/DayBackgroundMobile.png";
 import nightBg from "../assets/NightBackgroundMobile.png";
 
+import Ajib from "./Ajib";
+
 function Room() {
   const [isNight, setIsNight] = useState(false);
 
   return (
     <div
-      className="w-full h-dvh bg-cover bg-center duration-500"
+      className="w-full h-dvh bg-cover bg-center duration-500 relative overflow-hidden"
       style={{
         backgroundImage: `url(${isNight ? nightBg : dayBg})`,
       }}
     >
+
+
       {/* Toggle Button */}
       <button
         onClick={() => setIsNight(!isNight)}
-        className="absolute top-5 right-5 bg-black/40 text-white px-4 py-2 rounded-full backdrop-blur-md"
+        className="absolute top-5 right-5 bg-black/40 text-white px-4 py-2 rounded-full backdrop-blur-md z-50"
       >
         {isNight ? "☀️ Day" : "🌙 Night"}
       </button>
+
+            {/* Character */}
+      <Ajib />
     </div>
   );
 }
